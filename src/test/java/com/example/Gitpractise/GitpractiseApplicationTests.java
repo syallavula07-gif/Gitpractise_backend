7ce74ca5eb20a992079394224f39d70f86be1d71
syallavula07-gif <syallavula07@gmail.com>
2025-10-29 11:28:18 +0530

    second commit

---
+++ src/test/java/com/example/Gitpractise/GitpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gitpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
